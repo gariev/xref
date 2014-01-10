48dc7ad3f5a7448216905307bdfb1c76b714bfab
Tim Otten <totten@civicrm.org>
2014-01-09 21:53:21 -0800

    CLI Help - Output shortened program name

--- XRef.class.php
+++ XRef.class.php
@@ -1226,7 +1226,7 @@ class XRef {
     public static function showHelpScreen($tool_name, $usage_string = null) {
         global $argv;
         if (!$usage_string) {
-            $usage_string = "$argv[0] [options]";
+            $usage_string = basename($argv[0]) . " [options]";
         }
 
         echo "$tool_name, v. " . self::version() . "\n";
